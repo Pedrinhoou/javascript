fb3b1a5ba7f2f0273a7f5650507f2f029ec38b5e
Pedrinhoou <pedropaulolemes007@gmail.com>
2024-05-02 16:22:33 -0400

    exemplo de dados em js

---
+++ dados.js
@@ -0,0 +1,13 @@
+let nome = "Pedro";
+let verdadeiro = true;
+let falso = false;
+let numero = 17;
+let reais = 25.99;
+let nulo = null;
+let indefinido;
+let NotANumber = 0 / 0;
+let objeto = {
+     name: "Pedro",
+     age: 17,
+     profession: "estudante"
+}
