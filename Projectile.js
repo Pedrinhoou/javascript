a90c77cf0292281e448ce2b9b776d5ac0b2d689a
Pedro <pedropaulolemes007@gmail.com>
2024-05-28 16:14:39 -0400

    add draw function in Projectile class

--- Projectile.js
+++ Projectile.js
@@ -6,3 +6,10 @@ class Projectile {
         this.radius = 4;
     }
 }
+draw(); {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+}
